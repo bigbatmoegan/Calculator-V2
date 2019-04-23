604eda9ab76c9f0b16594b15cf5f80e086979050
bigbatmoegan <49653903+bigbatmoegan@users.noreply.github.com>
2019-04-23 17:16:10 -0400

    Update Eval_Expr_Tree.cpp

--- Eval_Expr_Tree.cpp
+++ Eval_Expr_Tree.cpp
@@ -8,55 +8,31 @@ Eval_Expr_Tree::Eval_Expr_Tree (void)
 
 void Eval_Expr_Tree::Visit_Add_Node ( Add_Expr_Node * node)
 {
-	// COMMENT: You should not be calling the eval () method on the
-	// composite in the visit method. Instead, the visit method should
-	// be performing the operation of the eval ().
-	/// I didnt see this comment until 10:00 PM on Monday, dont have time to change
 	result_=node->eval();
 }
 
 void Eval_Expr_Tree::Visit_Subtract_Node ( Subtract_Expr_Node * node)
 {
-	// COMMENT: You should not be calling the eval () method on the
-	// composite in the visit method. Instead, the visit method should
-	// be performing the operation of the eval ().
-	/// I didnt see this comment until 10:00 PM on Monday, dont have time to change
 	result_=node->eval();
 }
 
 void Eval_Expr_Tree::Visit_Multiplication_Node ( Multiplication_Expr_Node * node)
 {
-	// COMMENT: You should not be calling the eval () method on the
-	// composite in the visit method. Instead, the visit method should
-	// be performing the operation of the eval ().
-	/// I didnt see this comment until 10:00 PM on Monday, dont have time to change
 	result_=node->eval();
 }
 
 void Eval_Expr_Tree::Visit_Division_Node ( Division_Expr_Node * node)
 {
-	// COMMENT: You should not be calling the eval () method on the
-	// composite in the visit method. Instead, the visit method should
-	// be performing the operation of the eval ().
-	/// I didnt see this comment until 10:00 PM on Monday, dont have time to change
 	result_=node->eval();
 }
 
 void Eval_Expr_Tree::Visit_Modulus_Node ( Modulus_Expr_Node * node)
 {
-	// COMMENT: You should not be calling the eval () method on the
-	// composite in the visit method. Instead, the visit method should
-	// be performing the operation of the eval ().
-	/// I didnt see this comment until 10:00 PM on Monday, dont have time to change
 	result_=node->eval();
 }
 
 void Eval_Expr_Tree::Visit_Number_Node ( Number_Node * node)
 {
-	// COMMENT: You should not be calling the eval () method on the
-	// composite in the visit method. Instead, the visit method should
-	// be performing the operation of the eval ().
-	/// I didnt see this comment until 10:00 PM on Monday, dont have time to change
 	result_=node->eval();
 }
 
